3559efa91b1782d7053c1baa8eb400f79032097a
DanielOliyarnik <81031004+DanielOliyarnik@users.noreply.github.com>
2021-05-02 16:49:39 -0400

    new firejs

--- src/fire.js
+++ src/fire.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDgPndBMqAL9i8US2qJS0O6r3KP0gXDgHw",
-    authDomain: "geobot-6a616.firebaseapp.com",
-    projectId: "geobot-6a616",
-    storageBucket: "geobot-6a616.appspot.com",
-    messagingSenderId: "625767926144",
-    appId: "1:625767926144:web:8a4d7b9630c5b397cf1965",
-    measurementId: "G-PRVLZFE52X"
-  };
+  apiKey: "AIzaSyCbPiKtr1YHIAqX7R1VPbEO-DaSITat0Kk",
+  authDomain: "geo-bot-13214345436.firebaseapp.com",
+  projectId: "geo-bot-13214345436",
+  storageBucket: "geo-bot-13214345436.appspot.com",
+  messagingSenderId: "846162857790",
+  appId: "1:846162857790:web:99b55fcdb3abb2b2bfe66f",
+  measurementId: "G-V9JF82ESRN"
+};
 
 var database = firebase.initializeApp(firebaseConfig);
 
